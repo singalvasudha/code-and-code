94033b7b6ed30b531b256e00e4d3ad1a59b41163
singalvasudha <singalvasudha@gmail.com>
2017-07-10 00:14:49 +0530

    Create script.sql

---
+++ script.sql
@@ -0,0 +1,7 @@
+CREATE TABLE YAKSHOP.`herd` (
+  `yak_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `age` double DEFAULT NULL,
+  `sex` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`yak_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
